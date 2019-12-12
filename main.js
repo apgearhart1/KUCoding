c9da587bd2c7d7a787ad677454ac1db2a59b44da
apgearhart1 <apgearhart1@gmail.com>
2019-12-12 16:13:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let x = 10;
+let y = 101.3;
+let added = x + y;
+let subtracted = x - y;
+let multiplied = x * y;
+let divided = x / y;
+console.log(added, subtracted, multiplied, divided);
+
+let value = false;
+let value2 = true;
+let isEqual = !(x == y);
+console.log(isEqual);
